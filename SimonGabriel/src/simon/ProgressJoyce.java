f46cfee13ce0f6286cdea324b2cc5858bec84ac7
sidings <jrna.fng@gmail.com>
2017-01-09 21:58:02 -0500

    this one is done

--- SimonGabriel/src/simon/ProgressJoyce.java
+++ SimonGabriel/src/simon/ProgressJoyce.java
@@ -1,71 +1,68 @@
 package simon;
 
+import java.awt.Color;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 
-public class ProgressJoyce implements ProgressInterfaceGabriel {
+import guiPractice.components.Component;
 
+public class ProgressJoyce extends Component implements ProgressInterfaceGabriel {
+
+	private static final int WIDTH = 120;
+	private static final int HEIGHT = 50;
+
+	private boolean gameOver;
+	private String round;
+	private String sequence;
+	
 	public ProgressJoyce() {
+		super(60, 60, WIDTH, HEIGHT);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public int getHeight() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public BufferedImage getImage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getWidth() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getX() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getY() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public boolean isAnimated() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void update() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
 	public void setRound(int roundNumber) {
-		// TODO Auto-generated method stub
-
+		round = "Round "+roundNumber;
+		update();
 	}
 
 	@Override
 	public void setSequenceSize(int sequenceNumber) {
-		// TODO Auto-generated method stub
-
+		sequence = "Sequence length "+sequenceNumber;
+		update();
 	}
 
 	@Override
 	public void gameOver() {
-		// TODO Auto-generated method stub
+		gameOver = true;
+		update();
+	}
 
+	@Override
+	public void update(Graphics2D g) {
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		FontMetrics fm = g.getFontMetrics();
+		if(gameOver){
+			g.setColor(new Color(255,55,90));
+			g.fillRect(0, 0, WIDTH, HEIGHT);
+			g.setColor(Color.white);
+			String go = "GAME OVER!";
+			g.drawString(go, (WIDTH - fm.stringWidth(go))/2, 20);
+			g.drawString(sequence, (WIDTH - fm.stringWidth(sequence))/2, 40);
+
+		}else{
+			g.setColor(new Color(220,255,230));
+			g.fillRect(0, 0, WIDTH, HEIGHT);
+			g.setColor(Color.black);
+			g.drawRect(0, 0, WIDTH-1, HEIGHT-1);
+			if(round !=null && sequence != null){
+
+				g.drawString(round, (WIDTH - fm.stringWidth(round))/2, 20);
+				g.drawString(sequence, (WIDTH - fm.stringWidth(sequence))/2, 40);
+			}
+		}
 	}
 
 }
